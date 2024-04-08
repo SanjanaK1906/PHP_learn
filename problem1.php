d66d501c43df7d013a69215417670a8b474183cf
Sanjana Kumbhar <120375747+sanju1906@users.noreply.github.com>
2024-04-08 17:57:48 +0530

    Create problem1.php
    Write a PHP script to delay the program execution for the 5 seconds.

---
+++ problem1.php
@@ -0,0 +1,9 @@
+// Write a PHP script to delay the program execution for the 5 seconds.
+// Output the current time in the 'hour:minute:second' format
+echo date('h:i:s') . "\n";
+
+// Sleep for 5 seconds
+sleep(5);
+
+// Output the current time again after waking up
+echo date('h:i:s')."\n"
