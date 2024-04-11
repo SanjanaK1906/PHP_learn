ac49ddb991bdaf007bb5b9953c3f69b0174670e6
Sanjana Kumbhar <120375747+sanju1906@users.noreply.github.com>
2024-04-11 14:57:17 +0530

    Create swapping_1.php
    swap two numbers without using third variable

---
+++ swapping_1.php
@@ -0,0 +1,12 @@
+<?php
+//Swap value in two variales without using third variable
+//using addition and substraction
+$a=8;
+$b=1;
+$a = $a - $b;
+$b = $a + $b;
+$a = $b - $a;
+
+echo "value of a is ".$a." value of b is ".$b;
+
+?>
